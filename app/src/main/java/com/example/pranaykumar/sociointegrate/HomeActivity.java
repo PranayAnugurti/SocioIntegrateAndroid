90fb7af85c3957a3bb4fe5aae8de7f5635aec740
PranayAnugurti <pranaypandu.225@gmail.com>
2018-01-13 20:09:01 +0530

    Typing Implemented

--- app/src/main/java/com/example/pranaykumar/sociointegrate/HomeActivity.java
+++ app/src/main/java/com/example/pranaykumar/sociointegrate/HomeActivity.java
@@ -1,29 +1,25 @@
 package com.example.pranaykumar.sociointegrate;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
 import android.widget.Toast;
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.emitter.Emitter.Listener;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
-
+import java.net.URISyntaxException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URISyntaxException;
-
 public class HomeActivity extends AppCompatActivity {
 
-  private Button friendsBtn,groupChatBtn;
-
   public static Socket socket;
+  private Button friendsBtn, groupChatBtn;
 
   {
     try {
@@ -40,14 +36,14 @@ public class HomeActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
 
-    friendsBtn=(Button)findViewById(R.id.friendsTextView);
-    groupChatBtn=(Button)findViewById(R.id.groupChatTextView);
+    friendsBtn = (Button) findViewById(R.id.friendsTextView);
+    groupChatBtn = (Button) findViewById(R.id.groupChatTextView);
     final JSONObject sendText = new JSONObject();
     String user_id = null;
-    Constants constants  = new Constants();
+    Constants constants = new Constants();
     user_id = constants.user_id;
     try {
-      sendText.put("id",user_id);
+      sendText.put("id", user_id);
     } catch (JSONException e) {
       e.printStackTrace();
     }
@@ -57,49 +53,48 @@ public class HomeActivity extends AppCompatActivity {
     socket.on("connect", new Listener() {
       @Override
       public void call(Object... args) {
-       socket.emit("authentication",sendText);
+        socket.emit("authentication", sendText);
 
-       socket.on("authenticated", new Listener() {
-         @Override
-         public void call(Object... args) {
-           Constants.isConnected=true;
-           Log.d("LOG","isConnected:"+Constants.isConnected);
-           HomeActivity.socket.on("private",new Emitter.Listener() {
-             @Override
-             public void call(final Object... args) {
-               runOnUiThread(new Runnable() {
-                 @Override
-                 public void run() {
-                   JSONObject data = (JSONObject) args[0];
-                   String message = null;
-                   String from = null;
-                   String to = null;
-                   String time = null;
-                   try {
-                     message = data.getString("message").toString();
-                     from = data.getString("from").toString();
-                     to = data.getString("to").toString();
-                     time=data.getString("date").toString();
-                     Log.d("from Priivate Chat", message + " " + from + " " + to+time);
-                     Log.d("LOG", message);
-                   } catch (JSONException e) {
-                     e.printStackTrace();
-                   }
-                  //* addUser("other");
-                   //addMessage(message);*//*
-                 }
-               });
-             }
-           } );
+        socket.on("authenticated", new Listener() {
+          @Override
+          public void call(Object... args) {
+            Constants.isConnected = true;
+            Log.d("LOG", "isConnected:" + Constants.isConnected);
+            HomeActivity.socket.on("private", new Emitter.Listener() {
+              @Override
+              public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                  @Override
+                  public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String message = null;
+                    String from = null;
+                    String to = null;
+                    String time = null;
+                    try {
+                      message = data.getString("message").toString();
+                      from = data.getString("from").toString();
+                      to = data.getString("to").toString();
+                      time = data.getString("date").toString();
+                      Log.d("from Priivate Chat", message + " " + from + " " + to + time);
+                      Log.d("LOG", message);
+                    } catch (JSONException e) {
+                      e.printStackTrace();
+                    }
+                    //* addUser("other");
+                    //addMessage(message);*//*
+                  }
+                });
+              }
+            });
 
-         }
-       });
+          }
+        });
         socket.on("disconnect", new Listener() {
           @Override
           public void call(Object... args) {
-            Constants.isConnected=false;
-            disconnected();
-            Log.d("LOG","isConnected:"+Constants.isConnected);
+            Constants.isConnected = false;
+            Log.d("LOG", "isConnected:" + Constants.isConnected);
           }
         });
 
@@ -109,8 +104,8 @@ public class HomeActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
               @Override
               public void run() {
-                Constants.isConnected=false;
-                Log.d("LOG","isConnected:"+Constants.isConnected);
+                Constants.isConnected = false;
+                Log.d("LOG", "isConnected:" + Constants.isConnected);
               }
             });
           }
@@ -132,34 +127,37 @@ public class HomeActivity extends AppCompatActivity {
       }
     });*/
 
-
     friendsBtn.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
-        if(Constants.isConnected) {
+        if (Constants.isConnected == true) {
           Intent intent = new Intent(HomeActivity.this, FriendsActivity.class);
           intent.putExtra("user_id", getIntent().getStringExtra("user_id"));
           startActivity(intent);
+        } else {
+          Toast.makeText(getApplicationContext(), "Not Connected to Server", Toast.LENGTH_SHORT)
+              .show();
         }
 
       }
     });
 
-
-
     groupChatBtn.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
-        Intent intent=new Intent(HomeActivity.this,SocketActivity.class);
-        intent.putExtra("user_id",Constants.user_id);
-        startActivity(intent);
+        if (Constants.isConnected == true) {
+          Intent intent = new Intent(HomeActivity.this, SocketActivity.class);
+          intent.putExtra("user_id", Constants.user_id);
+          startActivity(intent);
+        } else {
+          Toast.makeText(getApplicationContext(), "Not Connected to Server", Toast.LENGTH_SHORT)
+              .show();
+        }
       }
     });
   }
 
-  private void disconnected() {
 
-  }
 
 
 }
--- app/src/main/java/com/example/pranaykumar/sociointegrate/PrivateChatFragment.java
+++ app/src/main/java/com/example/pranaykumar/sociointegrate/PrivateChatFragment.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,135 +39,234 @@ import java.util.List;
  */
 
 public class PrivateChatFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    String friend_id;
-    private EditText mInputMessageView;
-    private RecyclerView mMessagesView;
-    private ChatFragment.OnFragmentInteractionListener mListener;
-    private List<Message> mMessages = new ArrayList<Message>();
-    private List<User> mUsers = new ArrayList<User>();
-    private RecyclerView.Adapter mAdapter;
-    String url = "http://192.168.0.102:5000";
+  // TODO: Rename parameter arguments, choose names that match
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "param1";
+  private static final String ARG_PARAM2 = "param2";
 
-    public PrivateChatFragment() {
-        // Required empty public constructor
-    }
+  // TODO: Rename and change types of parameters
+  String friend_id;
+  private EditText mInputMessageView;
+  private RecyclerView mMessagesView;
+  private ChatFragment.OnFragmentInteractionListener mListener;
+  private List<Message> mMessages = new ArrayList<Message>();
+  private List<User> mUsers = new ArrayList<User>();
+  private RecyclerView.Adapter mAdapter;
+  String url = "http://192.168.0.102:5000";
+  private boolean mTyping = false;
+  private Handler mTypingHandler = new Handler();
+  private static final int TYPING_TIMER_LENGTH = 600;
 
-    @Nullable
+  public PrivateChatFragment() {
+    // Required empty public constructor
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_priavatechat, container, false);
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+    new PrivateMessagesAsyncTask().execute("");
+    friend_id = getActivity().getIntent().getStringExtra("friend_id");
+    HomeActivity.socket.on("private", handleIncomingMessages);
+    HomeActivity.socket.on("typing", onTyping);
+    HomeActivity.socket.on("stop typing", onStopTyping);
+  }
+
+  private Emitter.Listener handleIncomingMessages = new Emitter.Listener() {
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             Bundle savedInstanceState) {
-      return inflater.inflate(R.layout.fragment_priavatechat, container, false);
-    }
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        new PrivateMessagesAsyncTask().execute("");
-        HomeActivity.socket.on("private", handleIncomingMessages);
-
-    }
-
-    private Emitter.Listener handleIncomingMessages = new Emitter.Listener() {
+    public void call(final Object... args) {
+      getActivity().runOnUiThread(new Runnable() {
         @Override
-        public void call(final Object... args) {
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject data = (JSONObject) args[0];
-                    String message = null;
-                    String from = null;
-                    String to = null;
-                    String time = null;
-                    try {
-                        message = data.getString("message").toString();
-                        from = data.getString("from").toString();
-                        to = data.getString("to").toString();
-                        time=data.getString("date").toString();
-                        Log.d("from Priivate Chat", message + " " + from + " " + to+time);
-                        Log.d("LOG", message);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    addUser("other");
-                    addMessage(message);
-                }
-            });
+        public void run() {
+          JSONObject data = (JSONObject) args[0];
+          String message = null;
+          String from = null;
+          String to = null;
+          String time = null;
+          try {
+            message = data.getString("message").toString();
+            from = data.getString("from").toString();
+            to = data.getString("to").toString();
+            time = data.getString("date").toString();
+            Log.d("from Priivate Chat", message + " " + from + " " + to + time);
+            Log.d("LOG", message);
+          } catch (JSONException e) {
+            e.printStackTrace();
+          }
+          addUser("other");
+          addMessage(message);
         }
-    };
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mMessagesView = (RecyclerView) view.findViewById(R.id.messages);
-        mMessagesView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mAdapter = new MessageAdapter(mMessages, mUsers);
-        mMessagesView.setAdapter(mAdapter);
-        ImageButton sendButton = (ImageButton) view.findViewById(R.id.send_button);
-        mInputMessageView = (EditText) view.findViewById(R.id.message_input);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessage();
-            }
-        });
-        mAdapter.notifyDataSetChanged();
+      });
     }
+  };
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    mMessagesView = (RecyclerView) view.findViewById(R.id.messages);
+    mMessagesView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    mAdapter = new MessageAdapter(mMessages, mUsers);
+    mMessagesView.setAdapter(mAdapter);
+    final ImageButton sendButton = (ImageButton) view.findViewById(R.id.send_button);
+    mInputMessageView = (EditText) view.findViewById(R.id.message_input);
+    sendButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        sendMessage();
+      }
+    });
+    mAdapter.notifyDataSetChanged();
+    mInputMessageView = (EditText) view.findViewById(R.id.message_input);
+
+    mInputMessageView.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (null == Constants.user_id)
+          return;
+        if (!HomeActivity.socket.connected())
+          return;
+        JSONObject sendText = new JSONObject();
+        if (!mTyping) {
+          mTyping = true;
+          try {
+            sendText.put("from", Constants.user_id);
+            sendText.put("to", friend_id);
+          } catch (JSONException e) {
+            e.printStackTrace();
+          }
+
+          HomeActivity.socket.emit("typing", sendText);
+          Log.d("LOG","typing");
+        }
+
+        mTypingHandler.removeCallbacks(onTypingTimeout);
+        mTypingHandler.postDelayed(onTypingTimeout, TYPING_TIMER_LENGTH);
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+      }
+    });
+  }
+
+  private Emitter.Listener onTyping = new Emitter.Listener() {
+    @Override
+    public void call(final Object... args) {
+      getActivity().runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+
+          String username= (String) args[0];
+          addTyping(username);
+          Log.d("LOG",args[0]+"started typing");
+        }
+      });
+    }
+  };
+
+
+  private Emitter.Listener onStopTyping = new Emitter.Listener() {
+    @Override
+    public void call(final Object... args) {
+      getActivity().runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+
+          removeTyping((String)args[0]);
+          Log.d("LOG",args[0]+"stopped typing");
+        }
+      });
+    }
+  };
+
+  private Runnable onTypingTimeout = new Runnable() {
+    @Override
+    public void run() {
+      if (!mTyping)
+        return;
+
+      mTyping = false;
+      JSONObject sendText = new JSONObject();
+      if (!mTyping) {
+        mTyping = true;
+        try {
+          sendText.put("from", Constants.user_id);
+          sendText.put("to", friend_id);
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+
+        HomeActivity.socket.emit("stop typing", sendText);
+        Log.d("LOG","stop typing");
+      }
+    }
+  };
+
+    private void addTyping(String username) {
+
+    }
+
+    private void removeTyping(String username) {
+
+      }
 
     private void sendMessage() {
-        String message = mInputMessageView.getText().toString().trim();
-        mInputMessageView.setText("");
-        addMessage(message);
-        addUser("me");
-        JSONObject sendText = new JSONObject();
-        try {
+      String message = mInputMessageView.getText().toString().trim();
+      mInputMessageView.setText("");
+      addMessage(message);
+      addUser("me");
+      JSONObject sendText = new JSONObject();
+      try {
 
-            Constants constants = new Constants();
-            PrivateSocketActivity privateSocketActivity = new PrivateSocketActivity();
-            friend_id = getActivity().getIntent().getStringExtra("friend_id");
-            Log.d("from Private Chat", message + " " + constants.user_id + " " + friend_id);
-            sendText.put("message", message);
-            sendText.put("fromId", Constants.user_id);
-            sendText.put("to", friend_id);
-            HomeActivity.socket.emit("private", sendText);
-        } catch (JSONException e) {
+        Constants constants = new Constants();
+        PrivateSocketActivity privateSocketActivity = new PrivateSocketActivity();
+        friend_id = getActivity().getIntent().getStringExtra("friend_id");
+        Log.d("from Private Chat", message + " " + constants.user_id + " " + friend_id);
+        sendText.put("message", message);
+        sendText.put("fromId", Constants.user_id);
+        sendText.put("to", friend_id);
+        HomeActivity.socket.emit("private", sendText);
+      } catch (JSONException e) {
 
-        }
+      }
 
     }
 
 
     private void addUser(String user) {
-        mUsers.add(new User(user));
+      mUsers.add(new User(user));
 
     }
 
     private void addMessage(String message) {
 
-        mMessages.add(new Message(message,Constants.user_id,friend_id,""));
-        // mAdapter = new MessageAdapter(mMessages);
-        Log.d("LOG", "mMessages count=" + mMessages.size() + "message=" + message);
+      mMessages.add(new Message(message, Constants.user_id, friend_id, ""));
+      // mAdapter = new MessageAdapter(mMessages);
+      Log.d("LOG", "mMessages count=" + mMessages.size() + "message=" + message);
 
-        mAdapter.notifyDataSetChanged();
-        scrollToBottom();
+      mAdapter.notifyDataSetChanged();
+      scrollToBottom();
     }
 
     private void scrollToBottom() {
-        mMessagesView.scrollToPosition(mAdapter.getItemCount() - 1);
+      mMessagesView.scrollToPosition(mAdapter.getItemCount() - 1);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        //HomeActivity.socket.disconnect();
-      HomeActivity.socket.off("private");
-    }
+
+
   private class PrivateMessagesAsyncTask extends AsyncTask<String, Void, String> {
 
     @Override
@@ -183,7 +285,7 @@ public class PrivateChatFragment extends Fragment {
       wr.writeBytes(params[1]);
       wr.flush();
       wr.close();*/
-        Log.d("LOG",params[0]);
+        Log.d("LOG", params[0]);
         InputStream in = httpURLConnection.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(in);
 
@@ -207,18 +309,19 @@ public class PrivateChatFragment extends Fragment {
     @Override
     protected void onPostExecute(String result) {
       super.onPostExecute(result);
-      Log.e("LOG", "RESULT FROM SERVER:"+result); // this is expecting a response code to be sent from your server upon receiving the POST data
+      Log.e("LOG", "RESULT FROM SERVER:"
+          + result); // this is expecting a response code to be sent from your server upon receiving the POST data
       try {
-        JSONArray messagesArray=new JSONArray(result);
-        int i=0;
+        JSONArray messagesArray = new JSONArray(result);
+        int i = 0;
         while (i < messagesArray.length()) {
 
           JSONObject message = messagesArray.getJSONObject(i);
-          Message currentMessage=new Message(message.getString("message"),
+          Message currentMessage = new Message(message.getString("message"),
               message.getString("from"),
               message.getString("to"),
               message.getString("date"));
-          mMessages.add(i,currentMessage);
+          mMessages.add(i, currentMessage);
           i++;
         }
       } catch (JSONException e) {
@@ -227,4 +330,5 @@ public class PrivateChatFragment extends Fragment {
     }
   }
 }
+
 
